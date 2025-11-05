5d58e95b5744dd39b9ff3cd91fb95bb8f5edeac3
Gunjan Mishra <gunjanmishra096@gmail.com>
2025-11-06 01:22:58 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -9,4 +9,4 @@ app.get("/api/msg", (_, res) => {
     res.status(200).json({ msg: "hello deployment form the backend" })
 });
 
-app.listen(PORT, () => console.log(`app is running http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`app is running http://13.50.250.229:${PORT}`));
