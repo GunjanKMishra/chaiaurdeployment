0ead60882a21d8a621537b83490ff9b644e5a743
Gunjan Mishra <gunjanmishra096@gmail.com>
2025-11-06 00:17:45 +0530

    for revision the deployment using docker and aws

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [msg, setMsg] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/msg")
+      .then((data) => data.json())
+      .then((data) => {
+        setMsg(data.msg)
+      }).catch((err) => console.error(err.message))
+  }, [])
+  return (
+
+    <>
+      <h1>Welcome to chai and frontend</h1>
+      <h2>DATA: {msg}</h2>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 4000
+app.use(cors("*"));
+
+app.get("/api/msg", (_, res) => {
+    res.status(200).json({ msg: "hello deployment form the backend" })
+});
+
+app.listen(PORT, () => console.log(`app is running http://localhost:${PORT}`));
